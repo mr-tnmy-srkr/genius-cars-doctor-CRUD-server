f56c20a2d7dc05b5ee85bc297719737f75d3794f
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-10-26 19:17:56 +0530

    single data find for checkout

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 // console.log(process.env.DB_USER)
 
@@ -31,6 +31,19 @@ async function run() {
     app.get("/services", async (req, res) => {
       const cursor = serviceCollection.find();
       const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const options = {
+        // Sort matched documents in descending order by rating
+        // sort: { "imdb.rating": -1 },
+        // Include only the `title` and `imdb` fields in the returned document
+        projection: { title: 1, price: 1, service_id: 1 },
+      };
+     const result = await serviceCollection.findOne(query,options);
       res.send(result);
     });
 
