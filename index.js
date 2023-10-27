38a8dac876bc77c0757e13ffbfca321ba5560a55
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-10-27 16:29:22 +0530

    app.delete success

--- index.js
+++ index.js
@@ -49,23 +49,33 @@ async function run() {
     });
 
     //bookings
-//new focus start ===========================================================
+
+    //new focus start ===========================================================
     app.get("/bookings", async (req, res) => {
       // console.log(req.query);
       // console.log(req.query.email);
-      let query = {}
+      let query = {};
 
-      if(req.query?.email){
-        query = {email: req.query.email}
+      if (req.query?.email) {
+        query = { email: req.query.email };
       }
       const result = await bookingCollection.find(query).toArray();
       res.send(result);
+      //then go to Bookings.jsx line no 9
     });
-//new focus end =============================================================
+    //new focus end =============================================================
+
     app.post("/bookings", async (req, res) => {
       const booking = req.body;
       // console.log(booking);
       const result = await bookingCollection.insertOne(booking);
+      res.send(result);
+    });
+
+    app.delete("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookingCollection.deleteOne(query);
       res.send(result);
     });
 
